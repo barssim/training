db9e30318293214357138e78544518ed6f167420
Ahmed <ahmed.el-qarfaoui@techem.de>
2020-03-04 09:32:50 +0100

    removes file in a folder

---
+++ src/filesTraining/ReadFileExample.java
@@ -0,0 +1,38 @@
+package filesTraining;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ReadFileExample {
+
+	public static void
+			main(String[] args) {
+
+		File file = new File("/home/dei06752/myGitHub/training/spielwiese/src/filesTraining/FilesRemover.java");
+		FileInputStream fis = null;
+
+		try {
+			fis = new FileInputStream(file);
+
+			System.out.println("Total file size to read (in bytes) : "
+					+ fis.available());
+
+			int content;
+			while ((content = fis.read()) != -1) {
+				// convert to char and display it
+				System.out.print((char) content);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fis != null)
+					fis.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+}
