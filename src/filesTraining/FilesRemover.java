b9299104dff32da6b2a5e7e6e74eebc5f02eb507
Ahmed <ahmed.el-qarfaoui@techem.de>
2020-03-04 09:26:32 +0100

    removes file in a folder

---
+++ src/filesTraining/FilesRemover.java
@@ -0,0 +1,19 @@
+package filesTraining;
+
+import java.io.File;
+import java.util.Arrays;
+
+public class FilesRemover {
+	private void
+			remove(String FolderName) {
+		Arrays.stream(new File(FolderName).listFiles()).forEach(File::delete);
+	}
+
+	public static void
+			main(String[] args) {
+		FilesRemover filesRemover = new FilesRemover();
+		filesRemover.remove("./folderRemoverTest/");
+
+	}
+
+}
