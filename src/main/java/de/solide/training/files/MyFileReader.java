28b43118dc732678d64a3b8b53dd2f1fd85bd5a3
Ahmed <qarfaoui@gmail.com>
2021-11-26 21:08:47 +0100

    [B] refactoring package files

---
+++ src/main/java/de/solide/training/files/MyFileReader.java
@@ -0,0 +1,39 @@
+package de.solide.training.files;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class MyFileReader {
+
+
+	public static void main(String[] args) {
+		
+	        final String file = "Student.txt";
+	        String suchendeString = "Mark Sagal";
+	        String line = null;
+	        ArrayList<String> fileContents = new ArrayList<>();
+
+	        try {
+	            FileReader fReader = new FileReader(file);
+	            BufferedReader fileBuff = new BufferedReader(fReader);
+	            while ((line = fileBuff.readLine()) != null) {
+	                fileContents.add(line);
+	            }
+	            fileBuff.close();
+	        } catch (Exception e) {
+	            System.out.println(e.getMessage());
+	        }
+		for (String item : fileContents) {
+			if (suchendeString.equals(item)) {
+				System.out.println("String existe in File");
+				return;
+			}
+
+		}
+	
+		System.out.println("String not existe in File");
+		return;
+	       
+	}
+}
