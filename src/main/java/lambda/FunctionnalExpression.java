0f1b24783297d85c0a5218995a5ff6b9a020d4b5
barssim <qarfaoui@gmail.com>
2024-02-07 23:32:06 +0100

    lambda expression an Optional class

---
+++ src/main/java/lambda/FunctionnalExpression.java
@@ -0,0 +1,22 @@
+package lambda;
+
+interface FunctionInterfaceNoArgument {
+	public String greeting();
+}
+
+interface FunctionInterfaceOneArgument {
+	public int add(int i);
+}
+
+
+public class FunctionnalExpression {
+	public static void main(String[] args) {
+		
+		FunctionInterfaceNoArgument F0 = () -> { return "Hallo Ahmed"; };
+		System.out.println(F0.greeting());
+		
+
+		FunctionInterfaceOneArgument F1 = (number) -> number + 5;
+		System.out.println(F1.add(8)); //Result must be 13
+	}
+}
---
+++ src/main/java/lambda/OptionalClass.java
@@ -0,0 +1,20 @@
+package lambda;
+
+import java.util.Optional;
+
+public class OptionalClass {
+
+	public static void main(String[] args) {
+		String s1 = "test";
+		String s2 = null;
+		Optional<String> s = Optional.of("Testing");
+		System.out.println(s);
+		System.out.println(s.get());
+		System.out.println(Optional.empty());
+		
+		System.out.println(Optional.ofNullable(s1));
+		System.out.println(Optional.ofNullable(s2));
+				
+	}
+
+}
